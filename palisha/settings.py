6f8b221204b79c8f316381a625761e52b7723e2e
anishDev419 <manandharanish419@gmail.com>
2024-04-23 12:52:14 +0545

    Local Database Connection

--- palisha/settings.py
+++ palisha/settings.py
@@ -37,7 +37,7 @@ INSTALLED_APPS = [
     "django.contrib.sessions",
     "django.contrib.messages",
     "django.contrib.staticfiles",
-    "bootstrap5",
+    "django_bootstrap5",
     "products",
     "heroes",
     "orders",
@@ -78,9 +78,13 @@ WSGI_APPLICATION = "palisha.wsgi.application"
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
 DATABASES = {
-    "default": {
-        "ENGINE": "django.db.backends.sqlite3",
-        "NAME": BASE_DIR / "db.sqlite3",
+    'default': {
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'db_palisa',
+        'USER': 'anish',
+        'PASSWORD': '',
+        'HOST': 'localhost',  # Or your database host
+        'PORT': '3306',       # Or your database port
     }
 }
 
